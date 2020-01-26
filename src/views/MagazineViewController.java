9339209f3c28b09638150d466f6eb3425fbcd6ca
FGap <filgap@wp.pl>
2020-01-26 17:33:53 +0100

    extended data validation

--- src/views/MagazineViewController.java
+++ src/views/MagazineViewController.java
@@ -69,36 +69,87 @@ public class MagazineViewController implements Initializable {
     }
 
     public void findIngredient(ActionEvent actionEvent) {
+        int checking = 0;
         String regexToFind = findIngredientTextField.getText();
-        ArrayList<MagazineItem> itemsInDB = MagazineJdbcClass.getInstance().getItems();
-        ingredientsTableView.getItems().clear();
-        ObservableList<MagazineItemProperty> items= FXCollections.observableArrayList();
-        ArrayList<MagazineItem> regexItems = new ArrayList<>();
-        itemsInDB.forEach(position -> {if(position.getName().contains(regexToFind)) regexItems.add(position);});
-//        MagazineList.getInstance().getItemsInMagazineRegex(regexToFind)
-                regexItems.forEach(position -> items.add(new MagazineItemProperty(position.getAmount(), position.getName())));
-        ingredientsTableView.setItems(items);
-        Button showAllButton = JavaFXUtils.createButton("Pokaż wszystko.");
-        showAllButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                showIngredients();
-                topHBox.getChildren().remove(topHBox.getChildren().size() - 1);
-            }
-        });
-        topHBox.getChildren().add(showAllButton);
+        if (findIngredientTextField.getText() == null || findIngredientTextField.getText().trim().isEmpty()) {
+            views.ErrorBox.showError("Error", "Input can't be empty");
+            checking++;
+        }
         findIngredientTextField.clear();
+        if (app.data.DataValidation.checkSpecialChars(regexToFind) == "") {
+            views.ErrorBox.showError("Error", "There isn't any food with special characters");
+            checking++;
+        }
+        if (app.data.DataValidation.checkSize(32, regexToFind) == "") {
+            views.ErrorBox.showError("Error", "Keyword can't be longer than 32 characters");
+            checking++;
+        }
+        if(app.data.DataValidation.checkOnlyNumbers(regexToFind)==""){
+            views.ErrorBox.showError("Error", "There can't be numbers in ingredients names");
+            checking++;
+        }
+        if (checking == 0) {
+            ArrayList<MagazineItem> itemsInDB = MagazineJdbcClass.getInstance().getItems();
+            ingredientsTableView.getItems().clear();
+            ObservableList<MagazineItemProperty> items = FXCollections.observableArrayList();
+            ArrayList<MagazineItem> regexItems = new ArrayList<>();
+            itemsInDB.forEach(position -> {
+                if (position.getName().contains(regexToFind)) regexItems.add(position);
+            });
+//        MagazineList.getInstance().getItemsInMagazineRegex(regexToFind)
+            regexItems.forEach(position -> items.add(new MagazineItemProperty(position.getAmount(), position.getName())));
+            ingredientsTableView.setItems(items);
+            Button showAllButton = JavaFXUtils.createButton("Pokaż wszystko.");
+            showAllButton.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    showIngredients();
+                    topHBox.getChildren().remove(topHBox.getChildren().size() - 1);
+                }
+            });
+            if (topHBox.getChildren().size() == 3)
+                topHBox.getChildren().add(showAllButton);
+            findIngredientTextField.clear();
+        }
     }
 
     public void addIngredient(ActionEvent actionEvent) {
+        int checking = 0;
         String name = ingredientNameTextField.getText();
-        int amount = Integer.valueOf(ingredientAmountTextField.getText());
+        String amount_validation = ingredientAmountTextField.getText();
+        if (ingredientNameTextField.getText() == null || ingredientNameTextField.getText().trim().isEmpty()) {
+            views.ErrorBox.showError("Error", "Name input can't be empty");
+            checking++;
+        }
+        if (ingredientAmountTextField.getText() == null || ingredientAmountTextField.getText().trim().isEmpty()) {
+            views.ErrorBox.showError("Error", "Amount input can't be empty");
+            checking++;
+        }
+
+        if ( app.data.DataValidation.checkSpecialChars(name)=="" || app.data.DataValidation.checkSpecialChars(amount_validation)=="") {
+            views.ErrorBox.showError("Error", "Neither input can contain special characters");
+            checking++;
+        }
+
+        if (app.data.DataValidation.checkOnlyLetters(name)=="") {
+            views.ErrorBox.showError("Error", "Name input can't contain numbers");
+            checking++;
+        }
+
+        if (app.data.DataValidation.checkOnlyNumbers(amount_validation)=="") {
+            views.ErrorBox.showError("Error", "Amount input must be an integer");
+            checking++;
+        }
+
+        if(checking ==0) {
+            name = name.toLowerCase();
+            int amount = Integer.valueOf(ingredientAmountTextField.getText());
             MagazineItem newItem = new MagazineItem(amount, name);
             MagazineJdbcClass.getInstance().addItem(newItem);
             ingredientNameTextField.clear();
             ingredientAmountTextField.clear();
             showIngredients();
-
+        }
     }
 }
 
